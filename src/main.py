f434469d8b3389105124884c538904b6e84aa424
Haoxiang Yang <haoxiangyang89@gmail.com>
2022-02-28 10:22:49 +0800

    Add the initial code/data structure

---
+++ src/main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 22 01:01:06 2022
+
+@author: haoxiang
+"""
+
+'''
+main structure of running decomposition instances
+'''
---
+++ src/readin.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 22 00:58:01 2022
+
+@author: haoxiang
+"""
+
+'''
+Read in the data of production planning problem 
+'''
+
+# We need data structure for items, plants, inventory, transit, capacity
