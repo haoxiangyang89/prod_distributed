bf6f64d8a75792eaaa46012ff322fe1b18f4d4e4
Haoxiang Yang <haoxiangyang89@gmail.com>
2022-03-21 19:18:44 +0800

    Updated the readin structure

---
+++ src/extensive.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 00:58:01 2022
+
+@author: haoxiang
+"""
+
+'''
+Solve the extensive formulation for the multi-period production problem
+'''
--- src/readin.py
+++ src/readin.py
@@ -6,15 +6,28 @@ Created on Mon Feb 22 00:58:01 2022
 """
 
 '''
-Read in the data of production planning problem 
+Read in the data of production planning problem
 '''
 
 import pandas as pd
 
-# We need data structure for items, plants, inventory, transit, capacity
+# We need data structure for items, plants, calendar
 
 def readin_csv(file_add):
     return pd.read_csv(file_add)
+
+def input_bom(file_add, itemList):
+    # item bom relationship: read dm_df_bom
+    raw_data = readin_csv(file_add)
+
+    #       * dm_df_item_set: item capacity type
+    #       * dm_df_unit_capacity: item unit capacity consumption
+    #       * dm_df_transit: item transit relationship
+    #       * dm_df_inv: item initial inventory
+
+    # initial inventory
+
+    # unit capacity comsumption
 
 class prod_item():
     '''
@@ -22,8 +35,29 @@ class prod_item():
 
     '''
     def __init__(self):
+        # read in the raw data related to items:
+        #       * dm_df_item: item basic information
+        #       * other information added through other read-in functions
         pass
-    
-    def input_item(file_add):
-        # read in the raw data related to items
-        raw_data = readin_csv(file_add)
+
+class plant():
+    '''
+        Data structure for plants.
+
+    '''
+    def __init__(self):
+        # read in the raw data related to items:
+        #       * dm_df_item: item basic information
+        #       * other information added through other read-in functions
+        pass
+
+class calendar():
+    '''
+        Data structure for external purchase/demand.
+
+    '''
+    def __init__(self):
+        # read in the raw data related to items:
+        #       * dm_df_item: item basic information
+        #       * other information added through other read-in functions
+        pass
